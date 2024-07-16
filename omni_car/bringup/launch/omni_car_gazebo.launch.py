285bbf51c9db1b76d341631886afae0405eccc1d
Waleczna_Lama <rafal.krynski13@gmail.com>
2024-07-16 10:36:17 +0200

    rgb+depth cameras

--- omni_car/bringup/launch/omni_car_gazebo.launch.py
+++ omni_car/bringup/launch/omni_car_gazebo.launch.py
@@ -18,15 +18,32 @@ def generate_launch_description():
             description="Start RViz2 automatically with this launch file.",
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "rqt",
+            default_value="false",
+            description="Start rqt automatically with this launch file.",
+        )
+    )
 
     # Initialize Arguments
-    rviz = LaunchConfiguration("rviz")
+    arg_rviz = LaunchConfiguration("rviz")
+    arg_rqt = LaunchConfiguration("rqt")
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]), " ",
+            PathJoinSubstitution([FindPackageShare("omni_car"), "urdf", "omni_car.xacro"]), " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution([FindPackageShare("omni_car"), "rviz", "rviz.rviz"])
 
     # gazebo
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
-        ),
+        PythonLaunchDescriptionSource([FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]),
         launch_arguments={"gz_args": " -r -v 4 empty.sdf"}.items(),  # -v verbose level, -r run immediately 
     )
 
@@ -42,31 +59,14 @@ def generate_launch_description():
         ],
     )
 
-    # Get URDF via xacro
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution(
-                [FindPackageShare("omni_car"), "urdf", "omni_car.xacro"]
-            ),
-            " ",
-        ]
-    )
-    robot_description = {"robot_description": robot_description_content}
-
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("omni_car"), "rviz", "rviz.rviz"]
-    )
-
-    node_robot_state_publisher = Node(
+    robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
         parameters=[robot_description],
     )
 
-    node_joint_state_publisher = Node(
+    joint_state_publisher = Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
     )
@@ -88,28 +88,40 @@ def generate_launch_description():
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan',
-                   '/imu@sensor_msgs/msg/Imu@gz.msgs.IMU'],
+                   '/imu@sensor_msgs/msg/Imu@gz.msgs.IMU',
+                   '/camera@sensor_msgs/msg/Image@gz.msgs.Image',
+                   '/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
+                   '/depth_camera@sensor_msgs/msg/Image@gz.msgs.Image'
+                   ],
         output='screen'
     )
 
-    node_rviz = Node(
+    rviz = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="log",
         arguments=["-d", rviz_config_file],
-        condition=IfCondition(rviz),
+        condition=IfCondition(arg_rviz),
+    )
+
+    rqt = Node(
+        package='rqt_image_view',
+        executable='rqt_image_view',
+        arguments=['/camera'],
+        condition=IfCondition(arg_rqt),
     )
 
     nodes = [
         gazebo,
-        node_joint_state_publisher,
-        node_robot_state_publisher,
         gz_spawn_entity,
+        joint_state_publisher,
+        robot_state_publisher,
         joint_state_broadcaster_spawner,
         robot_controller_spawner,
         bridge,
-        node_rviz,
+        rviz,
+        rqt
     ]
 
     return LaunchDescription(declared_arguments + nodes)
